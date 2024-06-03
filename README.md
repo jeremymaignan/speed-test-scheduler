f5605b6c89c1f9f12e41cc2e731c272fb81a8b3e
Jérémy Maignan <jeremy.maignan@gmail.com>
2024-06-03 17:26:12 +0100

    add readme

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # speed-test-scheduler
-Command to run speed tests every 30min and log the results.
+A command to run speed tests every 30min and log the results in a CSV file.
+
+## Purpose
+
+I own a 4G box, and the connection speed is highly impacted by the local traffic.
+Typically the connection is significantly slower after working hours and on weekends.
+
+![Network Status Screenshot](https://github.com/jeremymaignan/speed-test-scheduler/blob/main/network_status_screenshot.png)
+
+This project aims to identify the days and times when the internet connection is at its slowest.
+## Usage
+
+```sh
+# Give execution permission
+chmod +x ./setup_cron.sh
+
+# Setup cron job
+./setup_cron.sh
+
+# Check the job has been setup
+crontab -l
+
+# To edit the job (destination file or frequency)
+crontab -e
+
+# Read results
+cat ~/network_quality_log.csv
+```
--- setup_cron.sh
+++ setup_cron.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
-(crontab -l; echo "*/30 * * * * (filename=\"$HOME/network_quality_log.csv\"; output=\$(networkquality); download=\$(echo \"\$output\" | grep 'Downlink capacity' | awk '{print \$3, \$4}'); upload=\$(echo \"\$output\" | grep 'Uplink capacity' | awk '{print \$3, \$4}'); datetime=\$(date '+\%d/\%m/\%Y \%H:\%M'); if [ ! -f \$filename ]; then echo \"DateTime, Download, Upload\" > \$filename; fi; echo \"\$datetime, \$download, \$upload\" >> \$filename)") | crontab -
+(crontab -l; \
+echo "*/30 * * * * (filename=\"$HOME/network_quality_log.csv\"; output=\$(networkquality); download=\$(echo \"\$output\" | grep 'Downlink capacity' | awk '{print \$3, \$4}'); upload=\$(echo \"\$output\" | grep 'Uplink capacity' | awk '{print \$3, \$4}'); datetime=\$(date '+\%d/\%m/\%Y \%H:\%M'); if [ ! -f \$filename ]; then echo \"DateTime, Download, Upload\" > \$filename; fi; echo \"\$datetime, \$download, \$upload\" >> \$filename)") | \
+crontab -
